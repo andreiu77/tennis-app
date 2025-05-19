a80d61060c0d95218ee9e136d9f8b31897490cb7
Andrei Ungureanu <andreiungureanu2014@yahoo.ro>
2025-05-19 12:18:16 +0300

    Update route.ts

--- src/app/api/top-rackets/route.ts
+++ src/app/api/top-rackets/route.ts
@@ -1,8 +1,7 @@
-import { NextApiRequest, NextApiResponse } from 'next';
 import prisma from "@/lib/prisma"
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
+export async function GET(req: NextRequest, res: NextResponse) {
     try {
         /*
         Top 10 racket brands with the most players:
